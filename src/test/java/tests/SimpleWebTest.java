a1bf301850195e2c13ea0bea0a6434dba2ba97f4
Dragan Zrnic <dragan.zrnic@blc.edu.ba>
2021-01-16 23:12:28 +0100

    Not working

--- src/test/java/tests/SimpleWebTest.java
+++ src/test/java/tests/SimpleWebTest.java
@@ -27,23 +27,18 @@ public class SimpleWebTest {
 
     @Test
     public void searchDuckDuckGo() {
-        // Load the page
         driver.get("https://www.duckduckgo.com");
 
-        // Enter search phrase
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search_form_input_homepage")));
         WebElement searchInput = driver.findElement(By.id("search_form_input_homepage"));
         searchInput.sendKeys("giant panda");
 
-        // Click search button
         WebElement searchButton = driver.findElement(By.id("search_button_homepage"));
         searchButton.click();
 
-        // Wait for results to appear
         wait.until(ExpectedConditions.titleContains("giant panda"));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.results_links_deep a.result__a")));
 
-        // Make sure each result contains the word "panda"
         List<WebElement> resultLinks = driver.findElements(By.cssSelector("div.results_links_deep a.result__a"));
         for (WebElement link : resultLinks) {
             assertTrue(link.getText().matches("(?i).*panda.*"));
